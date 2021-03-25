18f05d6d542eaccb858a28473977622a3e5c44c3
MeesterMatt <67811238+MrMatt757@users.noreply.github.com>
2021-03-24 19:12:38 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+/*
+* Name: Matthew Espinosa
+* Project: Word Statistics
+* Program: Can utilize command line to take a .txt file as the 2nd argument, and can display
+* the amount of words that are present in the file, utilizing whitespace as the delimiter. 
+*/
+
+#include <stdio.h>
+#include "wordsProcessorLL.h"
+
+int main(int argc, char* argv[]){
+    
+    // check if argument structure is correct
+    if(argc != 2){
+	printf("Usage: ./execName fileName\n");
+	return -1;
+    }
+
+    // open a file with argv[1] as the name and check if it is valid
+    FILE* txtFile = fopen(argv[1], "r");
+    if(txtFile == NULL){
+	printf("Invalid file name\n");
+	return -1;
+    }
+
+    LLnode* head = countWordsWithLinkedList(txtFile);		// returns head of list of words in a .txt file
+
+    printLineCountInFile(txtFile);
+
+    printWordsLinkedList(head, argv[1]);			// print words in linked list head of file argv[1]
+
+    int charCount = getCharCountInList(head);
+    printf("The total number of chars in the file is %d\n", charCount);
+
+    destroyWords(head);						// free list
+}
